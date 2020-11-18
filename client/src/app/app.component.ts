d686cc7392005b4543cf68216c4e768d19c5ebba
gps57 <glen@swiftbt.com>
2020-11-18 08:57:14 -0500

    Completed Frontend Walking Skeleton

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  // property "title" can be accessed in the html template by using double curly brackets {{title}}
+  title = 'Jobs Follower';
+  users: any;
+
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error);
+    })
+  }
+
+
+}
+
