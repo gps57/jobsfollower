4fef1bb2b2c690843eaa357087f2c90dee2efa79
gps57 <glen@swiftbt.com>
2020-11-20 14:15:13 -0500

    Added Top Nav Bar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,11 +5,13 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
 
 // this decorator says that this file is an Angular module
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
