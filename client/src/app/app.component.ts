c6b98e7f756062d898e3dea347396a5d7ade7bd8
gps57 <glen@swiftbt.com>
2020-11-23 14:22:48 -0500

    Hookup Register form and api

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -13,13 +13,9 @@ export class AppComponent implements OnInit {
   title = 'Jobs Follower';
   users: any;
 
-  constructor(
-    private http: HttpClient,
-    private accountService: AccountService
-    ) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit() {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -27,15 +23,5 @@ export class AppComponent implements OnInit {
     const user: User = JSON.parse(localStorage.getItem('user'));
     this.accountService.setCurrentUser(user);
   }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-      this.users = response;
-    }, error => {
-      console.log(error);
-    })
-  }
-
-
 }
 
