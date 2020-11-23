e639e706852042c96412200b61676d3c3ca7a7a2
gps57 <glen@swiftbt.com>
2020-11-23 17:52:30 -0500

    Applied a route guard for authentication

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,13 +5,21 @@ import { JobDetailComponent } from './jobs/job-detail/job-detail.component';
 import { JobListComponent } from './jobs/job-list/job-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'jobs', component: JobListComponent},
-  {path: 'jobs/:id', component: JobDetailComponent},
-  {path: 'lists', component: ListsComponent},
-  {path: 'messages', component: MessagesComponent},
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      {path: 'jobs', component: JobListComponent, canActivate: [AuthGuard]},
+      {path: 'jobs/:id', component: JobDetailComponent},
+      {path: 'lists', component: ListsComponent},
+      {path: 'messages', component: MessagesComponent},
+    ]
+  },
   {path: '**', component: HomeComponent, pathMatch: 'full'}
 ];
 
