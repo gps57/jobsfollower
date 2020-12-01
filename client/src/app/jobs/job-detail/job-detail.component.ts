efe9f0a8c9aa0a4db84d52a58ed15e1f061e24e5
gps57 <glen@swiftbt.com>
2020-12-01 17:20:10 -0500

    Added a JobDetails component

--- client/src/app/jobs/job-detail/job-detail.component.ts
+++ client/src/app/jobs/job-detail/job-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { pipe } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { Job } from 'src/app/_models/job';
+import { JobsService } from 'src/app/_services/jobs.service';
 
 @Component({
   selector: 'app-job-detail',
@@ -6,10 +11,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./job-detail.component.css']
 })
 export class JobDetailComponent implements OnInit {
+  job: Job;
 
-  constructor() { }
+  constructor(private jobService: JobsService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadJob();
   }
 
+  loadJob() {
+    this.jobService.getJob(Number(this.route.snapshot.paramMap.get('id'))).subscribe(job => {
+        this.job = job;
+      });
+  }
 }
