9ff6c83b2d4a48a820c87bc324cbf97a0c350a4d
gps57 <glen@swiftbt.com>
2020-12-09 15:33:41 -0500

    Styling form fields in edit component

--- client/src/app/jobs/job-edit/job-edit.component.ts
+++ client/src/app/jobs/job-edit/job-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Job } from 'src/app/_models/job';
@@ -10,8 +11,9 @@ import { JobsService } from 'src/app/_services/jobs.service';
   styleUrls: ['./job-edit.component.css']
 })
 export class JobEditComponent implements OnInit {
+  @ViewChild('editForm') editForm: NgForm;
   job: Job;
-  isExpired: boolean = false;
+  isExpired: boolean = false; // TODO: this property needs to be added to the Job entity.
 
   constructor(private jobService: JobsService, private route: ActivatedRoute, private toastr: ToastrService) { }
 
@@ -28,6 +30,7 @@ export class JobEditComponent implements OnInit {
   updateJob() {
     console.log(this.job);
     this.toastr.success("Job profile updated successfully");
+    this.editForm.reset(this.job);
   }
 
 }
