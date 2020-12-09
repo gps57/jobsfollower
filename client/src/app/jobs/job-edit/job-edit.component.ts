d5709ac4d16b31cb2240c243f02ba75328caa4b5
gps57 <glen@swiftbt.com>
2020-12-09 14:14:33 -0500

    Added form fields to edit component.

--- client/src/app/jobs/job-edit/job-edit.component.ts
+++ client/src/app/jobs/job-edit/job-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Job } from 'src/app/_models/job';
+import { JobsService } from 'src/app/_services/jobs.service';
 
 @Component({
   selector: 'app-job-edit',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./job-edit.component.css']
 })
 export class JobEditComponent implements OnInit {
+  job: Job;
+  isExpired: boolean = false;
 
-  constructor() { }
+  constructor(private jobService: JobsService, private route: ActivatedRoute, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.loadJob();
+  }
+
+  loadJob() {
+    this.jobService.getJob(Number(this.route.snapshot.paramMap.get('id'))).subscribe(job => {
+        this.job = job;
+      });
+  }
+
+  updateJob() {
+    console.log(this.job);
+    this.toastr.success("Job profile updated successfully");
   }
 
 }
