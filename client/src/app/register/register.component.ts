32c7576e7113988a0ada9962a7191f44fd7ce32a
gps57 <glen@swiftbt.com>
2020-12-24 16:07:23 -0500

    Wired up front end to api for registration

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -10,12 +11,12 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
   registerForm: FormGroup;
+  validationErrors: string[] = [];
 
   constructor(private accountService: AccountService,
     private toastr: ToastrService,
-    private fb: FormBuilder) { }
+    private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
     this.initializeForm();
@@ -26,10 +27,6 @@ export class RegisterComponent implements OnInit {
       username: ['', Validators.required],
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
-      // city: ['', Validators.required],
-      // state: ['', Validators.required],
-      // zip: ['', Validators.required],
-      // dateOfBirth: ['', Validators.required],
       password: ['', [Validators.required,
         Validators.minLength(4),
         Validators.maxLength(8)]],
@@ -45,14 +42,13 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
-    console.log(this.registerForm.value);
-    // this.accountService.register(this.model).subscribe(response => {
-    //   console.log(response);
-    //   this.cancel();
-    // }, error => {
-    //   console.log(error);
-    //   this.toastr.error(error.error);
-    // })
+    this.accountService.register(this.registerForm.value).subscribe(response => {
+      // When they register, you might asume they want to add a job that they want to follow
+      // so take them to the Add Job form.
+      this.router.navigateByUrl('job/add'); 
+    }, error => {
+      this.validationErrors = error;
+    })
   }
 
   cancel() {
