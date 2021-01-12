39f5adde98ccf56f0c3a0ebe4e517e69258011df
gps57 <glen@swiftbt.com>
2021-01-12 16:05:51 -0500

    Getting message from repo class

--- API/Helpers/JobParams.cs
+++ API/Helpers/JobParams.cs
@@ -1,16 +1,7 @@
 namespace API.Helpers
 {
-    public class JobParams
+    public class JobParams : PaginationParams
     {
-        private const int MaxPageSize = 50;
-        public int PageNumber { get; set; } = 1;
-        private int _pageSize = 10;
-        public int PageSize
-        { 
-            get => _pageSize; 
-            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
-        }
-
         public string Company { get; set; }
         public string Title { get; set; }
         public string OrderBy { get; set; } = "created";
