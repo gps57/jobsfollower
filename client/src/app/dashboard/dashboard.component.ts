f2d2b97029ec12e25fc31e50c20530934fa558cc
gps57 <glen@swiftbt.com>
2021-01-25 17:49:19 -0500

    Restructured statsList to include a value and a type.

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -7,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
   statsList = [
-    {value: 'applied', display: 'Total Jobs Applied For'},
-    {value: 'active', display: 'Total Jobs Active'},
-    {value: 'rejected', display: 'Total Jobs Rejected'},
-    {value: 'expired', display: 'Total Jobs Expired'}
+    {value: '0', type: 'applied', display: 'Total Jobs Applied For'},
+    {value: '1', type: 'active', display: 'Total Jobs Active'},
+    {value: '2', type: 'rejected', display: 'Total Jobs Rejected'},
+    {value: '3', type: 'expired', display: 'Total Jobs Expired'}
   ];
 
   constructor() { }
