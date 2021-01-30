0f4635d7d8c12f5ffe4bf66117c73ca23979abed
gps57 <glen@swiftbt.com>
2021-01-29 19:20:28 -0500

    Migrating database to support JobsStats info

---
+++ API/Data/Migrations/20210130000210_ExtendingJobEntity29Jan2021.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class ExtendingJobEntity29Jan2021 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateApplied",
+                table: "Jobs",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Responded",
+                table: "Jobs",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateApplied",
+                table: "Jobs");
+
+            migrationBuilder.DropColumn(
+                name: "Responded",
+                table: "Jobs");
+        }
+    }
+}
